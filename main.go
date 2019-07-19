ef2a8da5e19e9f1072fd6ffd01720be9b5ad375c
Ilia Laskov <Oce11oT@yandex.ru>
2019-07-19 15:30:19 +0300

    update

--- main.go
+++ main.go
@@ -1,20 +1,53 @@
 package main
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
+	"html/template"
+	"log"
 	"net/http"
 )
+type People struct {
+	Id int
+	Name string
+	Sex string
+	Age int
+}
+var database *sql.DB
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := database.Query("select * from people")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+	people := []People{}
+
+	for rows.Next(){
+		p := People{}
+		err := rows.Scan(&p.Id, &p.Name, &p.Sex, &p.Age)
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+		people = append(people, p)
+	}
+
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	tmpl.Execute(w, people)
+}
 
 func main() {
+	connStr := "user=postgres password=people1234 dbname=people sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		http.ServeFile(w, r, "./hello.html" )
-	})
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "About Page")
-	})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "Index Page")
-	})
-	fmt.Println("Server* is listening...")
+	if err != nil {
+		log.Println(err)
+	}
+	database = db
+	defer db.Close()
+	http.HandleFunc("/", IndexHandler)
+
+	fmt.Println("Server is listening...")
 	http.ListenAndServe(":8181", nil)
 }

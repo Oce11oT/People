b6f7412964ac082ca09b396b155e946515aaabfb
Ilia Laskov <Oce11oT@yandex.ru>
2019-07-19 14:53:46 +0300

    update

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		http.ServeFile(w, r, "hello.html" )
+		http.ServeFile(w, r, "./hello.html" )
 	})
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, "About Page")
@@ -15,6 +15,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, "Index Page")
 	})
-	fmt.Println("Server is listening...")
+	fmt.Println("Server* is listening...")
 	http.ListenAndServe(":8181", nil)
 }

8e00a598de04e211c30d1cf42f22835bae79073a
Ilia Laskov <Oce11oT@yandex.ru>
2019-07-19 14:44:10 +0300

    update

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		http.ServeFile(w, r, "hello")
+		http.ServeFile(w, r, "hello.html" )
 	})
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, "About Page")
